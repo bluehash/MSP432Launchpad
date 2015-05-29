2f2a8bf1a0c1fcd43ff5b3a29c2c44b8f259d6d0
bluehash@43oh <bluevault@gmail.com>
2015-05-29 10:47:45 -0400

    - Imitial import of source files.

---
+++ MSP432-Launchpad-Printf-Support/main.c
@@ -0,0 +1,99 @@
+/******************************************************************************
+ * MSP432 printf example project
+ *
+ * Description: An example project to illustrate printf usage on the MSP432
+ * Launchpad.
+
+ * Author: Gerard Sequeira, 43oh
+*******************************************************************************/
+
+/* DriverLib Includes */
+#include "driverlib.h"
+
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/* UART Configuration Parameter. These are the configuration parameters to
+ * make the eUSCI A UART module to operate with a 115200 baud rate. These
+ * values were calculated using the online calculator that TI provides
+ * at:
+ * http://processors.wiki.ti.com/index.php/
+ *               USCI_UART_Baud_Rate_Gen_Mode_Selection
+ */
+const eUSCI_UART_Config uartConfig =
+{
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+        26,                                      // BRDIV = 26
+        0,                                       // UCxBRF = 0
+        0,                                       // UCxBRS = 0
+        EUSCI_A_UART_NO_PARITY,                  // No Parity
+        EUSCI_A_UART_LSB_FIRST,                  // MSB First
+        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
+        EUSCI_A_UART_MODE,                       // UART mode
+        EUSCI_A_UART_LOW_FREQUENCY_BAUDRATE_GENERATION  // Low Frequency Mode
+};
+
+/*
+ * USCIA0 interrupt handler.
+ */
+void EusciA0_ISR(void)
+{
+    int receiveByte = UCA0RXBUF;
+
+    GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+
+    /* Echo back. */
+    EUSCI_A_UART_transmitData(EUSCI_A0_MODULE, receiveByte);
+
+    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+}
+
+int main(void)
+{
+    /* Halting WDT and disabling master interrupts */
+	WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
+
+    /* Initialize main clock to 3MHz */
+    MAP_CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_3);
+    CS_initClockSignal(CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+    CS_initClockSignal(CS_HSMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+    CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+
+    /* Selecting P1.0 as output (LED). */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
+        GPIO_PIN0, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+
+    /* Selecting P1.2 and P1.3 in UART mode. */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
+        GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Configuring UART Module */
+    MAP_UART_initModule(EUSCI_A0_MODULE, &uartConfig);
+
+    /* Enable UART module */
+    MAP_UART_enableModule(EUSCI_A0_MODULE);
+
+    UART_enableInterrupt(EUSCI_A0_MODULE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    Interrupt_enableInterrupt(INT_EUSCIA0);
+	Interrupt_enableMaster();
+
+	printf("\r\nPrintf support for the launchpad\r\n");
+
+	printf("Decimal(10) :%d\r\n", 10);
+	printf("Hex(10)     :%x\r\n", 10);
+	printf("float       :%f\r\n", 4.32);
+
+    /* Main while loop */
+	while(1)
+	{
+		MAP_PCM_gotoLPM0();
+	}
+}
+
---
+++ MSP432-Launchpad-Printf-Support/printfOverride.c
@@ -0,0 +1,43 @@
+/*
+ * printfOverride.c
+ *
+ *  Created on: May 27, 2015
+ *      Author: Gerard Sequeira, 43oh
+ *
+ *  With input from:
+ *  http://processors.wiki.ti.com/index.php/
+ *       Printf_support_for_MSP430_CCSTUDIO_compiler
+ */
+
+/* DriverLib Includes */
+#include "driverlib.h"
+
+/* Standard Includes */
+#include <stdio.h>
+#include <string.h>
+
+int fputc(int _c, register FILE *_fp);
+int fputs(const char *_ptr, register FILE *_fp);
+
+int fputc(int _c, register FILE *_fp)
+{
+  while(!(UCA0IFG&UCTXIFG));
+  UCA0TXBUF = (unsigned char) _c;
+
+  return((unsigned char)_c);
+}
+
+int fputs(const char *_ptr, register FILE *_fp)
+{
+  unsigned int i, len;
+
+  len = strlen(_ptr);
+
+  for(i=0 ; i<len ; i++)
+  {
+    while(!(UCA0IFG&UCTXIFG));
+    UCA0TXBUF = (unsigned char) _ptr[i];
+  }
+
+  return len;
+}
